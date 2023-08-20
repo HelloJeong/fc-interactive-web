1581d9a2055cca347c9471809aa51d44199880fe
HelloJeong <jeongyk92@gmail.com>
2023-08-20 15:43:39 +0900

    stick, tension

--- canvas/06_rope_physics/js/App.js
+++ canvas/06_rope_physics/js/App.js
@@ -1,3 +1,6 @@
+import Dot from "./Dot.js";
+import Stick from "./Stick.js";
+
 export default class App {
   static width = innerWidth;
   static height = innerHeight;
@@ -7,6 +10,10 @@ export default class App {
   constructor() {
     this.canvas = document.querySelector("canvas");
     this.ctx = this.canvas.getContext("2d");
+
+    this.dots = [new Dot(400, 50), new Dot(500, 100), new Dot(600, 50), new Dot(800, 50)];
+    this.sticks = [new Stick(this.dots[0], this.dots[1]), new Stick(this.dots[1], this.dots[2]), new Stick(this.dots[2], this.dots[3])];
+    this.dots[0].pinned = true;
 
     this.resize();
     window.addEventListener("resize", this.resize.bind(this));
@@ -37,7 +44,16 @@ export default class App {
       then = now - (delta % App.interval);
 
       this.ctx.clearRect(0, 0, App.width, App.height);
-      this.ctx.fillRect(100, 100, 100, 100);
+
+      this.dots.forEach((dot) => {
+        dot.update();
+        dot.draw(this.ctx);
+      });
+
+      this.sticks.forEach((stick) => {
+        stick.update();
+        stick.draw(this.ctx);
+      });
     };
     requestAnimationFrame(frame);
   }
---
+++ canvas/06_rope_physics/js/Dot.js
@@ -0,0 +1,34 @@
+import Vector from "./Vector.js";
+
+export default class Dot {
+  constructor(x, y) {
+    this.pos = new Vector(x, y);
+    this.oldPos = new Vector(x, y);
+
+    this.gravity = new Vector(0, 1);
+    this.friction = 0.97;
+
+    this.pinned = false;
+
+    this.mass = 1;
+  }
+  update() {
+    if (this.pinned) return;
+
+    let vel = Vector.sub(this.pos, this.oldPos);
+
+    this.oldPos.setXY(this.pos.x, this.pos.y);
+
+    vel.mult(this.friction);
+
+    vel.add(this.gravity);
+    this.pos.add(vel);
+  }
+  draw(ctx) {
+    ctx.fillStyle = "#000";
+    ctx.beginPath();
+    ctx.arc(this.pos.x, this.pos.y, 10, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.closePath();
+  }
+}
