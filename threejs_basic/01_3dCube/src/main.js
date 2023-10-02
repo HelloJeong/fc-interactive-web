f8830bec4f09db0feddff852c96b3c2e99620609
HelloJeong <jeongyk92@gmail.com>
2023-10-02 17:09:39 +0900

    resize

--- threejs_basic/01_3dCube/src/main.js
+++ threejs_basic/01_3dCube/src/main.js
@@ -17,13 +17,29 @@ function init() {
   const scene = new THREE.Scene();
 
   // 물체에 대한 원근감을 표현할 수 있는 카메라
-  // (시야각, 카메라의 종횡비, near(가까이), far(멀리))
+  // (fov(시야각), 카메라의 종횡비, near(가까이), far(멀리))
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 500);
 
   const geometry = new THREE.BoxGeometry(2, 2, 2);
 
   // const material = new THREE.MeshBasicMaterial({ color: 0xcc99ff }); // 조명에 영향을 받지 않음, 빛에 의한 음영이 표시 안됨
-  const material = new THREE.MeshStandardMaterial({ color: 0xcc99ff });
+  const material = new THREE.MeshStandardMaterial({
+    color: 0xcc99ff,
+    // color: new THREE.Color(0xcc99ff),
+
+    // transparent: true, // 이게 있어야 opacity 같은게 적용 됨
+    // opacity: 0.5,
+
+    // visible: false,
+
+    // wireframe: true,
+
+    // side: THREE.FrontSide, // default,
+    // side: THREE.BackSide,
+    // side: THREE.DoubleSide, // 양면을 보여주려면 리소스를 더 사용하게 됨
+  });
+
+  material.color = new THREE.Color(0x00c896);
 
   const cube = new THREE.Mesh(geometry, material);
 
@@ -46,4 +62,17 @@ function init() {
   scene.add(ambientLight);
 
   renderer.render(scene, camera);
+
+  function handleResize() {
+    // 카메라의 종횡비도 같이 변경시켜줘야함
+    camera.aspect = window.innerWidth / window.innerHeight;
+
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    renderer.render(scene, camera);
+  }
+
+  window.addEventListener("resize", handleResize);
 }
